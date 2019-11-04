6c2c71cc5356a57e6f9eebd88003225ebdc09247
Chuan Sun <chuansun.sc@gmail.com>
2019-11-04 14:33:04 -0600

    detailed documentation on NN

--- src/regression.py
+++ src/regression.py
@@ -7,25 +7,25 @@ from joblib import dump
 from keras.models import load_model, model_from_json
 
 
-#final_input = pd.read_csv("../data/processed/final.csv")
+final_input = pd.read_csv("../data/processed/final.csv")
 
 '''
 Some data cleaning
 '''
-# final_input.dropna(subset=['score1', 'score2'], inplace=True)
-# columns_to_dummy = ['league', 'team1', 'team2']
-# df = pd.get_dummies(final_input, columns=columns_to_dummy)
-#
-# num_pipeline = Pipeline([
-#     ('imputer', SimpleImputer(strategy="median")),
-#     ('std_scaler', StandardScaler()),
-# ])
-# df_columns = df.columns.to_list()
-# df_columns.remove('score1')
-# df_columns.remove('score2')
-# df[df_columns] = num_pipeline.fit_transform(df[df_columns])
-# print "processed data:{}".format(df)
-# df.to_csv("../data/processed/training.csv")
+final_input.dropna(subset=['score1', 'score2'], inplace=True)
+columns_to_dummy = ['league', 'team1', 'team2']
+df = pd.get_dummies(final_input, columns=columns_to_dummy)
+
+num_pipeline = Pipeline([
+    ('imputer', SimpleImputer(strategy="median")),
+    ('std_scaler', StandardScaler()),
+])
+df_columns = df.columns.to_list()
+df_columns.remove('score1')
+df_columns.remove('score2')
+df[df_columns] = num_pipeline.fit_transform(df[df_columns])
+print "processed data:{}".format(df)
+df.to_csv("../data/processed/training.csv")
 
 
 def prepare_data_for_predict_testset():
@@ -92,34 +92,34 @@ def train_n_evaluate(model, model_name, cv=3):
 '''
 Linear Regression
 '''
-# from sklearn.linear_model import SGDRegressor
-# print "fitting SGD regressor..."
-# sgd_reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty=None, eta0=0.1)
-# train_n_evaluate(sgd_reg, "linear_regression", cv=5)
-# #  scores:[-8.32199806e+24 -5.97997859e+24 -8.19218360e+24]
+from sklearn.linear_model import SGDRegressor
+print "fitting SGD regressor..."
+sgd_reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty=None, eta0=0.1)
+train_n_evaluate(sgd_reg, "linear_regression", cv=5)
+#  scores:[-8.32199806e+24 -5.97997859e+24 -8.19218360e+24]
 
 '''
 Polynomial regression
 '''
-# from sklearn.preprocessing import PolynomialFeatures
-# print "fitting polynomial regressor..."
-# poly_features = PolynomialFeatures(degree=2, include_bias=False)
-# X_poly = poly_features.fit_transform(X_train)
-# lin_reg = LinearRegression()
-# lin_reg.fit(X_poly, y_train1)
-# print "polynomial regressor trained."
-# scores = cross_val_score(lin_reg, X_poly, y_train1)
-# print "scores:{}".format(scores)
+from sklearn.preprocessing import PolynomialFeatures
+print "fitting polynomial regressor..."
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X_train)
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y_train1)
+print "polynomial regressor trained."
+scores = cross_val_score(lin_reg, X_poly, y_train1)
+print "scores:{}".format(scores)
 
 '''
 Random Forest
 '''
 from sklearn.ensemble import RandomForestRegressor
-# rf_reg = RandomForestRegressor(n_estimators=40, verbose=1, criterion='mse')
-# train_n_evaluate(rf_reg, "random_forest", cv=5)
-# # cross val scores for score1:[0.54887999 0.55289514 0.58159345 0.57460962 0.55528305 0.57258883 0.55020498 0.56175289 0.57008246 0.5858311 ]
-# # cross val scores for score2:[0.55198016 0.5520537  0.5223262  0.56590597 0.53978307 0.57134336 0.54745679 0.56139037 0.53256986 0.56298315 ]
-# # 40 estimators with MSE: [0.56336969 0.54826698 0.54049093 0.56902241 0.54205725 0.58976756]
+rf_reg = RandomForestRegressor(n_estimators=40, verbose=1, criterion='mse')
+train_n_evaluate(rf_reg, "random_forest", cv=5)
+# cross val scores for score1:[0.54887999 0.55289514 0.58159345 0.57460962 0.55528305 0.57258883 0.55020498 0.56175289 0.57008246 0.5858311 ]
+# cross val scores for score2:[0.55198016 0.5520537  0.5223262  0.56590597 0.53978307 0.57134336 0.54745679 0.56139037 0.53256986 0.56298315 ]
+# 40 estimators with MSE: [0.56336969 0.54826698 0.54049093 0.56902241 0.54205725 0.58976756]
 
 
 '''
@@ -181,30 +181,15 @@ def create_keras_model(activation='selu', optimizer='adam', neuron=50, init='lec
     for i in range(3):
         layer = Dense(units=neuron, activation=activation, kernel_initializer=init)(layer)
 
-    # _ = Dense(units=200, activation='selu', kernel_initializer='lecun_normal', )(_)
-    # _ = Dense(units=200, activation='selu', kernel_initializer='lecun_normal', )(_)
-    # _ = Dense(units=200, activation='selu', kernel_initializer='lecun_normal', )(_)
-    # _ = Dense(units=200, activation='selu', kernel_initializer='lecun_normal', )(_)
-    # _ = Dense(units=200, activation='selu', kernel_initializer='lecun_normal', )(_)
-
-    # _ = Dense(units=50, activation='sigmoid')(_)
-
-    # _ = Dense(units=100, activation='relu')(_)
-    score1_output = Dense(units=1, activation='relu', name='score1_output')(layer)
-    # score2 output
-    #_ = Dense(units=100, activation='selu', kernel_initializer='lecun_normal', input_shape=X_train.shape[1:])(input_layer)
-    #_ = Dense(units=100, activation='selu', kernel_initializer='lecun_normal', )(_)
-    # _ = Dense(units=100, activation='relu')(_)
-    #score2_output = Dense(units=1, activation='linear', name='score2_output')(_)
-    # nn_model = Model(inputs=input_layer, outputs=[score1_output, score2_output]) # for multi-output
-    nn_model = Model(inputs=input_layer, outputs=[score1_output])
+    score_output = Dense(units=1, activation='relu', name='score_output')(layer)
+    nn_model = Model(inputs=input_layer, outputs=[score_output])
 
     # nn_model.compile(optimizer='adam',
     #                  loss={'score1_output': 'mse', 'score2_output': 'mse'},
     #                  metrics={'score1_output': coeff_determination, 'score2_output': coeff_determination})
     nn_model.compile(optimizer=optimizer,
                      loss='mse',
-                     metrics={'score1_output': coeff_determination})
+                     metrics={'score_output': coeff_determination})
     return nn_model
     # print nn_model.summary()
 
@@ -212,16 +197,16 @@ def create_keras_model(activation='selu', optimizer='adam', neuron=50, init='lec
 '''
 Run Grid Search to find best hyperparameters for score1
 '''
-# model = KerasRegressor(build_fn=create_keras_model, epochs=30)
-# activations = ['relu', 'sigmoid', 'linear']
-# # learn_rate = [0.001, 0.01, 0.1, 0.2, 0.3]
-# neurons = [50, 100, 200]
-# inits = ["lecun_normal", "he_normal", "lecun_uniform"]
-# optimizers = ["sgd", "adam", "nadam"]
-# param_grid = dict(activation=activations, optimizer=optimizers, neuron=neurons, init=inits)
-# grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
-# grid_result = grid.fit(X_train, y_train1)
-# print "Best: {} if using {}".format(grid_result.best_score_, grid_result.best_params_)
+model = KerasRegressor(build_fn=create_keras_model, epochs=30)
+activations = ['relu', 'sigmoid', 'linear']
+# learn_rate = [0.001, 0.01, 0.1, 0.2, 0.3]
+neurons = [50, 100, 200]
+inits = ["lecun_normal", "he_normal", "lecun_uniform"]
+optimizers = ["sgd", "adam", "nadam"]
+param_grid = dict(activation=activations, optimizer=optimizers, neuron=neurons, init=inits)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+grid_result = grid.fit(X_train, y_train1)
+print "Best: {} if using {}".format(grid_result.best_score_, grid_result.best_params_)
 
 
 def fit_model(score):
@@ -254,13 +239,10 @@ def load_model_n_predict(score, row_value_array):
     print "prediction for {} :{}".format(score, prediction)
 
 
-# rows = prepare_data_for_prediction()
-# load_model_n_predict('score1', rows)
-# load_model_n_predict('score2', rows)
-
 rows = prepare_data_for_predict_set()
-load_model_n_predict('score1', rows[4])
-load_model_n_predict('score2', rows[4])
+for row in rows:
+    load_model_n_predict('score1', row)
+    load_model_n_predict('score2', row)
 
 
 
